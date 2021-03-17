09e093a40ed001bc4af55efcb234a49a91d0ae9d
Bruce <34617983+brucelu2018@users.noreply.github.com>
2021-03-18 01:38:40 +0800

    Update MicroPayment.sol

--- smartcontracts/MicroPayment.sol
+++ smartcontracts/MicroPayment.sol
@@ -3,8 +3,6 @@
 pragma solidity ^0.8.2;
 
 interface IRewardToken {
-    function burn(address sender, uint256 amount) external;
-    function mint(address receiver, uint256 amount) external;
     function transferTo(address _from, address _to, uint _value) external;
     function balanceUnsettledOf(address who) external view returns (uint, uint);
 }
