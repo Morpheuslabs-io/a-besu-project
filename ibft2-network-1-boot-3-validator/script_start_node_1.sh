9161e27b2f8f267302a850bfd023ad3917aed31e
trung <>
2021-03-18 01:44:14 +0700

    Config for gas-free txs

--- ibft2-network-1-boot-3-validator/script_start_node_1.sh
+++ ibft2-network-1-boot-3-validator/script_start_node_1.sh
@@ -1,1 +1,1 @@
-./besu-21.1.2/bin/besu --data-path=./node-1/data --genesis-file=./genesis.json --rpc-http-enabled --rpc-http-api=ETH,NET,IBFT --host-allowlist="*" --rpc-http-cors-origins="all"
+./besu-21.1.2/bin/besu --data-path=./node-1/data --genesis-file=./genesis.json --rpc-http-enabled --rpc-http-api=ETH,NET,IBFT --host-allowlist="*" --rpc-http-cors-origins="all" --min-gas-price=0
