23379a3943caebe64329d47a88b85ce86c64f061
Bruce <34617983+brucelu2018@users.noreply.github.com>
2021-03-25 10:14:34 +0800

    Update Utility.sol

--- smartcontracts/contracts/utility/Utility.sol
+++ smartcontracts/contracts/utility/Utility.sol
@@ -9,17 +9,18 @@ import "./Transaction.sol";
 contract Utility {
     address public owner;
     string public merchant;
-    constructor(address _owner, string memory _merchant) public {
-        owner = _owner;
-        merchant = _merchant;
+    
+    constructor() public {
+        owner = msg.sender;
     }
     event Transacted (address debtor, unit256 index);
+    
 
     /*
-    * records of utilitization of each address
+    * records of transactions of each merchant address
     */
 
-    mapping (address => Transaction[]) public utility;
+    mapping (address => Transaction[]) public merchantTransaction;
 
     /*
     * records of utilitization of each address
@@ -30,19 +31,32 @@ contract Utility {
     /*
     * assign utility to an address
     */
+    
+    transaction
 
-    function add(bytes32 memory sku, uint256 price, uint256 quantity, address debtor, bytes32 memory posId,
-      , bytes32 memory description, uint256 memory timestamp) public {
+    function add(bytes32[] sku, uint256[] price, uint256[] quantity, bytes32[] description, bytes32 orderId,
+                        address debtor, bytes32 memory posId, uint256 totalAmount, uint256 memory timestamp) public {
         Transaction transaction = new Transaction();
-        transaction.
-        item.sku = sku;
-        item.price = price;
-        item.quantity = quantity;
-        item.posID = posId;
-        item.description = description;
-        item.timestamp = timestamp;
-        utility[debtor].push(item);
-        dept[debtor] += price;
+
+
+        
+        
+        for (uint i = 0; i < sku.length; i++) {
+            Item item = new Item();
+
+            item.sku = sku;
+            item.price = price;
+            item.quantity = quantity;
+            item.description = description; 
+            transaction.add(item);
+        }
+
+        transaction.posId = posId;
+        transaction.orderId = orderId;
+        transaction.total = totalAmount;        
+        transaction.timestamp = timestamp;
+        }
+
         emit Transacted(debtor, utility[debtor].length+1);
       }
 
