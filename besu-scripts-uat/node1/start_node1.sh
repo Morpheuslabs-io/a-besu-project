c857bf85dff805909b22183e1268d85825bc995e
trung <>
2021-03-31 22:33:58 +0700

    In the start script of each node, auto-create the keypair file from which can fetch the enodeID and also write it into a file "enode_id"

--- besu-scripts-uat/node1/start_node1.sh
+++ besu-scripts-uat/node1/start_node1.sh
@@ -1,18 +1,32 @@
 echo "Kill running node"
 
-if [ ! -z "$(sudo lsof -t -i:4545)" ]
+if [ ! -z "$(lsof -t -i:4545)" ]
 then
-  sudo kill -9 $(sudo lsof -t -i:4545)
+  kill -9 $(lsof -t -i:4545)
 fi
 
 echo "Delete existing node folder"
 
+# Without sudo, get "Permission denied"
 sudo rm -rf /node1
+
+echo "Delete old keypair"
+sudo rm -rf ./data/key
+sudo rm -rf ./data/key.pub
+
+echo "Creating keypair"
+
+# Do not use sudo here
+enode_key=$(/opt/besu-21.1.2/bin/besu --data-path=./data public-key export-address --to=./data/key.pub | grep -oE "0x[A-Fa-f0-9]*" | sed 's/0x//')
+
+echo "enode://$enode_key@127.0.0.1:30303" >> ./enode_id
 
 echo "Copy node folder to the target location"
 
+# Without sudo, get "Permission denied"
 sudo cp -rf ../node1 /node1
 
 echo "Starting node1 as boot node"
 
+# Without sudo, get "Permission denied"
 sudo /opt/besu-21.1.2/bin/besu --data-path /node1/data --genesis-file=/node1/genesis.json --config-file="/node1/config.toml" --host-allowlist="*" --rpc-http-cors-origins="all"
