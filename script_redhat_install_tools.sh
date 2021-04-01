ee7192578301f49b959a1c59baccd89f82a72039
trung <>
2021-04-01 20:29:38 +0700

    Update

--- script_redhat_install_tools.sh
+++ script_redhat_install_tools.sh
@@ -6,6 +6,10 @@ sudo dnf install git
 
 sudo yum install wget
 
+sudo yum install lsof
+
+sudo yum install vim
+
 sudo yum install unzip
 
 wget https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/21.1.2/besu-21.1.2.zip
