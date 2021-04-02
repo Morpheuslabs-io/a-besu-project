5168433a6b9240f2d53a9f62d1e74699b4d4572b
trung <>
2021-04-02 23:11:44 +0700

    Update

---
+++ besu-scripts-uat-not-work/node2/restart_node2.sh
@@ -0,0 +1,6 @@
+export P2P_HOST="0.0.0.0"
+export P2P_PORT="30303"
+export NODE_FOLDER="/node2"
+
+echo "Restarting $NODE_FOLDER as boot node"
+/opt/besu-21.1.2/bin/besu --data-path $NODE_FOLDER/data --genesis-file=$NODE_FOLDER/genesis.json --config-file=$NODE_FOLDER/config.toml --host-allowlist="*" --rpc-http-cors-origins="all" --p2p-host=$P2P_HOST --p2p-port=$P2P_PORT &
---
+++ besu-scripts-uat/node3/start_node.sh
@@ -0,0 +1,19 @@
+export P2P_HOST="0.0.0.0"
+export P2P_PORT="30303"
+export NODE_FOLDER="/node3"
+
+echo "Starting $NODE_FOLDER as validator node"
+
+echo "Delete old keypair"
+rm -rf ./data/key
+rm -rf ./data/key.pub
+
+echo "Creating keypair"
+enode_key=$(/opt/besu-21.1.2/bin/besu --data-path=./data public-key export-address --to=./data/key.pub | grep -oE "0x[A-Fa-f0-9]*" | sed 's/0x//')
+
+echo "enode://$enode_key@$P2P_HOST:$P2P_PORT" > ./enode_id
+
+echo "Copy node folder artifacts to the target location"
+cp -rf ..$NODE_FOLDER $NODE_FOLDER
+
+/opt/besu-21.1.2/bin/besu --data-path $NODE_FOLDER/data --genesis-file=$NODE_FOLDER/genesis.json --config-file=$NODE_FOLDER/config.toml --host-allowlist="*" --rpc-http-cors-origins="all" --p2p-host=$P2P_HOST --p2p-port=$P2P_PORT &
