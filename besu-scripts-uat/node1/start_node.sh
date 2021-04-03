271524f0fa6442e4bd3dab5e1a2553efc341d3f5
trung <>
2021-04-03 23:19:21 +0700

    Update

--- besu-scripts-uat/node1/start_node.sh
+++ besu-scripts-uat/node1/start_node.sh
@@ -7,4 +7,4 @@ echo "Starting $NODE_FOLDER as boot node"
 echo "Copy node folder artifacts to the target location"
 cp -rf ..$NODE_FOLDER $NODE_FOLDER
 
-/opt/besu-21.1.2/bin/besu --data-path $NODE_FOLDER/data --genesis-file=$NODE_FOLDER/genesis.json --config-file=$NODE_FOLDER/config.toml --host-allowlist="*" --rpc-http-cors-origins="all" --p2p-port=$P2P_PORT &
+/opt/besu-21.1.2/bin/besu --data-path $NODE_FOLDER/data --genesis-file=$NODE_FOLDER/genesis.json --config-file=$NODE_FOLDER/config.toml --host-allowlist="*" --rpc-http-cors-origins="all" --p2p-port=$P2P_PORT --p2p-host=$P2P_HOST &
