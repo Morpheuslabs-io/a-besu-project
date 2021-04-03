3e6888a62c2e1e89692ecc58c6ce94e1aa04159b
trung <>
2021-04-03 23:03:03 +0700

    delete

--- besu-scripts-uat-not-work/utils/get_enode.sh
+++
@@ -1,7 +0,0 @@
-# Listenning port of the node
-export NODE_PORT="4545"
-
-# Host of the node
-export NODE_HOST="http://127.0.0.1"
-
-curl -X POST --data '{"jsonrpc":"2.0","method":"net_enode","params":[],"id":1}' $NODE_HOST:$NODE_PORT
