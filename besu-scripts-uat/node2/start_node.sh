a1e512ca200f34a7f4cf16a9a9222fff92bfb489
trung <>
2021-04-06 17:25:51 +0700

    Enough 4 nodes

--- besu-scripts-uat/node2/start_node.sh
+++ besu-scripts-uat/node2/start_node.sh
@@ -5,21 +5,25 @@ if [ "$1" = "prod" ]; then
   nodeConfigFile="node.config.prod"
   tomlConfigFile="config.prod.toml"
   keypairDataFolder="data.prod"
+  bootnodeConfigFile="../bootnode.enode.prod"
 else
   if [ "$1" = "uat" ]; then
     echo "uat enviroment"
     nodeConfigFile="node.config.uat"
     tomlConfigFile="config.uat.toml"
     keypairDataFolder="data.uat"
+    bootnodeConfigFile="../bootnode.enode.uat"
   else
     echo "dev enviroment"
     nodeConfigFile="node.config.dev"
     tomlConfigFile="config.dev.toml"
     keypairDataFolder="data.dev"
+    bootnodeConfigFile="../bootnode.enode.dev"
   fi
 fi
 
 export $(egrep -v '^#' $nodeConfigFile | xargs)
+export $(egrep -v '^#' $bootnodeConfigFile | xargs)
 
 ###
 
@@ -34,4 +38,4 @@ cp -rf ..$NODE_FOLDER/$keypairDataFolder $NODE_FOLDER
 cp -rf ..$NODE_FOLDER/genesis.json $NODE_FOLDER
 cp -rf ..$NODE_FOLDER/*.sh $NODE_FOLDER
 
-/opt/besu-21.1.2/bin/besu --data-path $NODE_FOLDER/$keypairDataFolder --genesis-file=$NODE_FOLDER/genesis.json --config-file=$NODE_FOLDER/$tomlConfigFile --p2p-port=$P2P_PORT --p2p-host=$P2P_HOST --rpc-http-port=$RPC_PORT --bootnodes=$BOOT_NODE_1_ENODE &
+/opt/besu-21.1.2/bin/besu --data-path $NODE_FOLDER/$keypairDataFolder --genesis-file=$NODE_FOLDER/genesis.json --config-file=$NODE_FOLDER/$tomlConfigFile --p2p-port=$P2P_PORT --p2p-host=$P2P_HOST --rpc-http-port=$RPC_PORT --bootnodes=$BOOT_NODE_ENODE_LIST &
