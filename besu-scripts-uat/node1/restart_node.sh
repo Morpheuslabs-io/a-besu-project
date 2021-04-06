5676b4d1b8e2741a0922d223bb94e93a258fc0c8
trung <>
2021-04-06 22:42:31 +0700

    Update node1

--- besu-scripts-uat/node1/restart_node.sh
+++ besu-scripts-uat/node1/restart_node.sh
@@ -23,7 +23,6 @@ export $(egrep -v '^#' $nodeConfigFile | xargs)
 
 ###
 
-export $(egrep -v '^#' $nodeConfigFile | xargs)
-
 echo "Restarting $NODE_FOLDER as boot node"
-/opt/besu-21.1.2/bin/besu --data-path $NODE_FOLDER/data --genesis-file=$NODE_FOLDER/genesis.json --config-file=$NODE_FOLDER/config.toml --host-allowlist="*" --rpc-http-cors-origins="all" --p2p-host=$P2P_HOST --p2p-port=$P2P_PORT --rpc-http-port=$RPC_PORT &
+
+/opt/besu-21.1.2/bin/besu --data-path $NODE_FOLDER/data --genesis-file=$NODE_FOLDER/genesis.json --config-file=$NODE_FOLDER/$tomlConfigFile &
