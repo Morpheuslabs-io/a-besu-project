5090680d0cde5ee83d67f8d793e60398dbd59814
trung <>
2021-04-06 22:38:09 +0700

    Fix node1

--- besu-scripts-uat/node1/start_node.sh
+++ besu-scripts-uat/node1/start_node.sh
@@ -4,18 +4,18 @@ if [ "$1" = "prod" ]; then
   echo "prod enviroment"
   nodeConfigFile="node.config.prod"
   tomlConfigFile="config.prod.toml"
-  keypairDataFolder="data.prod"
+  keypairDataFolder="nodekey.prod"
 else
   if [ "$1" = "uat" ]; then
     echo "uat enviroment"
     nodeConfigFile="node.config.uat"
     tomlConfigFile="config.uat.toml"
-    keypairDataFolder="data.uat"
+    keypairDataFolder="nodekey.uat"
   else
     echo "dev enviroment"
     nodeConfigFile="node.config.dev"
     tomlConfigFile="config.dev.toml"
-    keypairDataFolder="data.dev"
+    keypairDataFolder="nodekey.dev"
   fi
 fi
 
@@ -30,8 +30,8 @@ rm -rf $NODE_FOLDER
 mkdir $NODE_FOLDER
 cp -rf ..$NODE_FOLDER/$nodeConfigFile $NODE_FOLDER
 cp -rf ..$NODE_FOLDER/$tomlConfigFile $NODE_FOLDER
-cp -rf ..$NODE_FOLDER/$keypairDataFolder $NODE_FOLDER
+cp -rf ..$NODE_FOLDER/$keypairDataFolder/* $NODE_FOLDER
 cp -rf ..$NODE_FOLDER/genesis.json $NODE_FOLDER
 cp -rf ..$NODE_FOLDER/*.sh $NODE_FOLDER
 
-/opt/besu-21.1.2/bin/besu --data-path $NODE_FOLDER/$keypairDataFolder --genesis-file=$NODE_FOLDER/genesis.json --config-file=$NODE_FOLDER/$tomlConfigFile --p2p-port=$P2P_PORT --p2p-host=$P2P_HOST --rpc-http-port=$RPC_PORT &
+/opt/besu-21.1.2/bin/besu --data-path $NODE_FOLDER/data --genesis-file=$NODE_FOLDER/genesis.json --config-file=$NODE_FOLDER/$tomlConfigFile &
