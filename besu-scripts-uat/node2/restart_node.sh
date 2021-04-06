94238aedda046dfb04cfa78475c23f5d255ddd48
trung <>
2021-04-06 22:53:09 +0700

    Fix node2

--- besu-scripts-uat/node2/restart_node.sh
+++ besu-scripts-uat/node2/restart_node.sh
@@ -5,29 +5,24 @@ if [ "$1" = "prod" ]; then
   nodeConfigFile="node.config.prod"
   tomlConfigFile="config.prod.toml"
   keypairDataFolder="data.prod"
-  bootnodeConfigFile="../bootnode.enode.prod"
 else
   if [ "$1" = "uat" ]; then
     echo "uat enviroment"
     nodeConfigFile="node.config.uat"
     tomlConfigFile="config.uat.toml"
     keypairDataFolder="data.uat"
-    bootnodeConfigFile="../bootnode.enode.uat"
   else
     echo "dev enviroment"
     nodeConfigFile="node.config.dev"
     tomlConfigFile="config.dev.toml"
     keypairDataFolder="data.dev"
-    bootnodeConfigFile="../bootnode.enode.dev"
   fi
 fi
 
 export $(egrep -v '^#' $nodeConfigFile | xargs)
-export $(egrep -v '^#' $bootnodeConfigFile | xargs)
 
 ###
 
-export $(egrep -v '^#' $nodeConfigFile | xargs)
-
 echo "Restarting $NODE_FOLDER as boot node"
-/opt/besu-21.1.2/bin/besu --data-path $NODE_FOLDER/data --genesis-file=$NODE_FOLDER/genesis.json --config-file=$NODE_FOLDER/config.toml --host-allowlist="*" --rpc-http-cors-origins="all" --p2p-host=$P2P_HOST --p2p-port=$P2P_PORT --rpc-http-port=$RPC_PORT --bootnodes=$BOOT_NODE_ENODE_LIST &
+
+/opt/besu-21.1.2/bin/besu --data-path $NODE_FOLDER/data --genesis-file=$NODE_FOLDER/genesis.json --config-file=$NODE_FOLDER/$tomlConfigFile &
