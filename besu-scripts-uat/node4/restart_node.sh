3c172bcc729219c571cf98ebed14cba277114367
trung <>
2021-04-06 23:11:27 +0700

    node3 and node4

--- besu-scripts-uat/node4/restart_node.sh
+++ besu-scripts-uat/node4/restart_node.sh
@@ -4,20 +4,20 @@ if [ "$1" = "prod" ]; then
   echo "prod enviroment"
   nodeConfigFile="node.config.prod"
   tomlConfigFile="config.prod.toml"
-  keypairDataFolder="data.prod"
+  keypairDataFolder="nodekey.prod"
   bootnodeConfigFile="../bootnode.enode.prod"
 else
   if [ "$1" = "uat" ]; then
     echo "uat enviroment"
     nodeConfigFile="node.config.uat"
     tomlConfigFile="config.uat.toml"
-    keypairDataFolder="data.uat"
+    keypairDataFolder="nodekey.uat"
     bootnodeConfigFile="../bootnode.enode.uat"
   else
     echo "dev enviroment"
     nodeConfigFile="node.config.dev"
     tomlConfigFile="config.dev.toml"
-    keypairDataFolder="data.dev"
+    keypairDataFolder="nodekey.dev"
     bootnodeConfigFile="../bootnode.enode.dev"
   fi
 fi
@@ -27,7 +27,6 @@ export $(egrep -v '^#' $bootnodeConfigFile | xargs)
 
 ###
 
-export $(egrep -v '^#' $nodeConfigFile | xargs)
+echo "Restarting $NODE_FOLDER as validator node"
 
-echo "Restarting $NODE_FOLDER as boot node"
-/opt/besu-21.1.2/bin/besu --data-path $NODE_FOLDER/data --genesis-file=$NODE_FOLDER/genesis.json --config-file=$NODE_FOLDER/config.toml --host-allowlist="*" --rpc-http-cors-origins="all" --p2p-host=$P2P_HOST --p2p-port=$P2P_PORT --rpc-http-port=$RPC_PORT --bootnodes=$BOOT_NODE_ENODE_LIST &
+/opt/besu-21.1.2/bin/besu --data-path $NODE_FOLDER/data --genesis-file=$NODE_FOLDER/genesis.json --config-file=$NODE_FOLDER/$tomlConfigFile --bootnodes=$BOOT_NODE_ENODE_LIST &
--- besu-scripts-uat/node4/start_node.sh
+++ besu-scripts-uat/node4/start_node.sh
@@ -4,20 +4,20 @@ if [ "$1" = "prod" ]; then
   echo "prod enviroment"
   nodeConfigFile="node.config.prod"
   tomlConfigFile="config.prod.toml"
-  keypairDataFolder="data.prod"
+  keypairDataFolder="nodekey.prod"
   bootnodeConfigFile="../bootnode.enode.prod"
 else
   if [ "$1" = "uat" ]; then
     echo "uat enviroment"
     nodeConfigFile="node.config.uat"
     tomlConfigFile="config.uat.toml"
-    keypairDataFolder="data.uat"
+    keypairDataFolder="nodekey.uat"
     bootnodeConfigFile="../bootnode.enode.uat"
   else
     echo "dev enviroment"
     nodeConfigFile="node.config.dev"
     tomlConfigFile="config.dev.toml"
-    keypairDataFolder="data.dev"
+    keypairDataFolder="nodekey.dev"
     bootnodeConfigFile="../bootnode.enode.dev"
   fi
 fi
@@ -27,15 +27,15 @@ export $(egrep -v '^#' $bootnodeConfigFile | xargs)
 
 ###
 
-echo "Starting $NODE_FOLDER as boot node"
+echo "Starting $NODE_FOLDER as validator node"
 
 echo "Copy node folder artifacts to the target location"
 rm -rf $NODE_FOLDER
 mkdir $NODE_FOLDER
 cp -rf ..$NODE_FOLDER/$nodeConfigFile $NODE_FOLDER
 cp -rf ..$NODE_FOLDER/$tomlConfigFile $NODE_FOLDER
-cp -rf ..$NODE_FOLDER/$keypairDataFolder $NODE_FOLDER
+cp -rf ..$NODE_FOLDER/$keypairDataFolder/* $NODE_FOLDER
 cp -rf ..$NODE_FOLDER/genesis.json $NODE_FOLDER
 cp -rf ..$NODE_FOLDER/*.sh $NODE_FOLDER
 
-/opt/besu-21.1.2/bin/besu --data-path $NODE_FOLDER/$keypairDataFolder --genesis-file=$NODE_FOLDER/genesis.json --config-file=$NODE_FOLDER/$tomlConfigFile --p2p-port=$P2P_PORT --p2p-host=$P2P_HOST --rpc-http-port=$RPC_PORT --bootnodes=$BOOT_NODE_ENODE_LIST &
+/opt/besu-21.1.2/bin/besu --data-path $NODE_FOLDER/data --genesis-file=$NODE_FOLDER/genesis.json --config-file=$NODE_FOLDER/$tomlConfigFile --bootnodes=$BOOT_NODE_ENODE_LIST &
