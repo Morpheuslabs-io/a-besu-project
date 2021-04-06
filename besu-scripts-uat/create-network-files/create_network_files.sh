4fa1195be9318d15924b1e5c21f2d7b1b8689cc2
Bruce Lu Yang <bruce@morpheuslabs.io>
2021-04-06 23:08:58 +0800

    update besu script structure and names

--- besu-scripts-uat/create-network-files/create_network_files.sh
+++ besu-scripts-uat/create-network-files/create_network_files.sh
@@ -2,4 +2,4 @@ export NETWORK_FILES_LOCATION="./networkFiles"
 
 rm -rf $NETWORK_FILES_LOCATION
 
-/opt/besu-21.1.2/bin/besu operator generate-blockchain-config --config-file=./ibftConfigFile.json --to=$NETWORK_FILES_LOCATION --private-key-file-name=key
+/opt/besu-21.1.2/bin/besu operator generate-blockchain-config --config-file=./ibftConfigFile.json --to=$NETWORK_FILES_LOCATION --private-key-file-name=nodekey
