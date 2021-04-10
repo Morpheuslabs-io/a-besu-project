a299185fb848080c2dd45ec0a783ba38e3e359b5
trung <>
2021-04-10 15:32:01 +0700

    Refactor deployContract_RewardToken

--- smartcontracts/deployment/deployContractNew.js
+++ smartcontracts/deployment/deployContractNew.js
@@ -1,6 +1,8 @@
 const axios = require("axios");
 const axiosRetry = require("axios-retry");
 const web3Utils = require("web3-utils");
+const fs = require("fs");
+const solc = require("solc");
 
 axiosRetry(axios, { retries: 3 });
 
@@ -9,11 +11,13 @@ const API_DEPLOY_CONTRACT = "http://127.0.0.1:30303/orchard/deployContract";
 const API_INVOKE_CONTRACT_METHOD =
   "http://127.0.0.1:30303/orchard/invokeContractMethod";
 
-async function deployContractWrapper(
+const sender = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1";
+
+async function request_deployContract(
   senderLabel,
   binary,
-  encodedConstructor,
-  contractAbi
+  contractAbi,
+  encodedConstructor
 ) {
   const result = await axios.post(API_DEPLOY_CONTRACT, {
     senderLabel,
@@ -21,7 +25,89 @@ async function deployContractWrapper(
     encodedConstructor,
     contractAbi,
   });
-  console.log("deployContractWrapper - result:", result.data);
+
+  return result.data;
+}
+
+async function compileContract(contractFolder, contractName, ctorArgs) {
+  let sourceFile = `${contractFolder}/${contractName}.sol`;
+  console.log(
+    `Start compiling contract ${contractName}, source file -->`,
+    sourceFile
+  );
+  let source;
+
+  let compilerOption = {
+    language: "Solidity",
+    settings: {
+      outputSelection: {
+        "*": {
+          "*": ["*"],
+        },
+      },
+    },
+  };
+
+  try {
+    source = fs.readFileSync(sourceFile, "utf8");
+  } catch (err) {
+    console.log("File not found", sourceFile);
+    return;
+  }
+
+  try {
+    compilerOption = {
+      ...compilerOption,
+      sources: { [contractName]: { content: source } },
+    };
+
+    let compiledContract = JSON.parse(
+      solc.compile(JSON.stringify(compilerOption))
+    );
+
+    compiledContract = compiledContract.contracts[contractName][contractName];
+
+    let bytecode = compiledContract.evm.bytecode.object;
+    let abi = compiledContract.abi;
+
+    return { bytecode, abi };
+  } catch (e) {
+    console.log("compileContract - Error:", e);
+    return null;
+  }
+}
+
+async function deployContract_RewardToken() {
+  try {
+    const symbolName = "RewardToken";
+    const decimal = 0;
+    const symbol = "RDT";
+    const totalSupply = web3Utils.toBN(2 * 10 ** 9 * 10 ** decimal); // 2 billions token, decimal 0;
+
+    const constructorArgumentsRewardToken = [
+      totalSupply,
+      symbolName,
+      symbol,
+      decimal,
+    ];
+    const compiledRewardToken = await compileContract(
+      "../contracts/micropayment",
+      "RewardToken",
+      constructorArgumentsRewardToken
+    );
+
+    const deployedRewardToken = await request_deployContract(
+      sender,
+      compiledRewardToken.bytecode,
+      compiledRewardToken.abi,
+      constructorArgumentsRewardToken
+    );
+
+    return deployedRewardToken;
+  } catch (e) {
+    console.error("deployContract_RewardToken - Error:", e);
+    return null;
+  }
 }
 
 async function invokeContractMethodWrapper(
@@ -39,38 +125,16 @@ async function invokeContractMethodWrapper(
 }
 
 async function main() {
-  // Test data
-  const senderLabel = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1";
-
-  const jsonFileRewardToken = require("../build/contracts/RewardToken.json");
-  const contractBytecodeRewardToken = jsonFileRewardToken.bytecode;
-  const contractAbiRewardToken = jsonFileRewardToken.abi;
-
-  const symbolName = "RewardToken";
-  const decimal = 0;
-  const symbol = "RDT";
-  const totalSupply = web3Utils.toBN(2 * 10 ** 9 * 10 ** decimal); // 2 billions token, decimal 0;
-
-  const constructorArgumentsRewardToken = [
-    totalSupply,
-    symbolName,
-    symbol,
-    decimal,
-  ];
-  ////////////
-
-  const deployContractRewardToken = await deployContractWrapper(
-    senderLabel,
-    contractBytecodeRewardToken,
-    constructorArgumentsRewardToken,
-    contractAbiRewardToken
-  );
+  const deployedRewardToken = await deployContract_RewardToken();
+  console.log("deployedRewardToken: ", deployedRewardToken);
 
   // const invokeContractMethodResult = await invokeContractMethodWrapper(
   //   senderLabel,
   //   contractMethodPayload,
   //   contractAddress
   // );
+
+  process.exit(0);
 }
 
 main();
