3dd23b2ff8f05a3448b89a30c29ebfc2735c6041
trung <>
2021-04-23 13:08:14 +0700

    Fixes

--- besu-scripts/node4/start_node.sh
+++ besu-scripts/node4/start_node.sh
@@ -5,25 +5,29 @@ if [ "$1" = "prod" ]; then
   nodeConfigFile="node.config.prod"
   tomlConfigFile="config.prod.toml"
   keypairDataFolder="nodekey.prod"
+  bootnodeConfigFile="../bootnode.enode.prod"
 else
   if [ "$1" = "uat" ]; then
     echo "uat enviroment"
     nodeConfigFile="node.config.uat"
     tomlConfigFile="config.uat.toml"
     keypairDataFolder="nodekey.uat"
+    bootnodeConfigFile="../bootnode.enode.uat"
   else
     echo "dev enviroment"
     nodeConfigFile="node.config.dev"
     tomlConfigFile="config.dev.toml"
     keypairDataFolder="nodekey.dev"
+    bootnodeConfigFile="../bootnode.enode.dev"
   fi
 fi
 
 export $(egrep -v '^#' $nodeConfigFile | xargs)
+export $(egrep -v '^#' $bootnodeConfigFile | xargs)
 
 ###
 
-echo "Starting $NODE_FOLDER as boot node"
+echo "Starting $NODE_FOLDER as validator node"
 
 echo "Copy node folder artifacts to the target location"
 
@@ -34,4 +38,4 @@ cp -rf ./genesis.json $NODE_FOLDER
 cp -rf ./besu-log.xml $NODE_FOLDER
 cp -rf ./*.sh $NODE_FOLDER
 
-LOG4J_CONFIGURATION_FILE=./besu-log.xml /opt/besu-21.1.2/bin/besu --data-path $NODE_FOLDER/data --genesis-file=$NODE_FOLDER/genesis.json --config-file=$NODE_FOLDER/$tomlConfigFile &
+LOG4J_CONFIGURATION_FILE=./besu-log.xml /opt/besu-21.1.2/bin/besu --data-path $NODE_FOLDER/data --genesis-file=$NODE_FOLDER/genesis.json --config-file=$NODE_FOLDER/$tomlConfigFile --bootnodes=$BOOT_NODE_ENODE_LIST &
